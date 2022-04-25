7c1012402e99c8dea9ce54407e71f3a8bac4cd96
Lasse Herzog <herzogl.tin21@student.dhbw-heidenheim.de>
2022-04-25 10:20:25 +0200

    Added documentation for drawline function

--- src/bitmap_generator_lib/drawing_utils.h
+++ src/bitmap_generator_lib/drawing_utils.h
@@ -4,14 +4,16 @@
 #define RASTERGRAFIK_GENERATOR_DRAWING_UTILS_H
 
 /**
- *
- * @param x0
- * @param y0
- * @param x1
- * @param y1
- * @param bitmap
- * @param rgb
- * @return
+ * Draws a line in a specified color in a bitmap struct using the Bresenham algorithm
+ * and the setPixel() function.
+ * @param x0 X-coordinate of start point
+ * @param y0 Y-coordinate of start point
+ * @param x1 X-coordinate of end point
+ * @param y1 Y-coordinate of end point
+ * @param bitmap Bitmap struct in which the line will be drawn.
+ * @param rgb Color in which the line will be drawn.
+ * @return 0 if line was successfully drawn
+ *         1 if start or end point is out of bounds of bitmap
  */
 extern int drawLine(int x0, int y0, int x1, int y1, Bitmap *bitmap, RGB rgb);
 
@@ -21,7 +23,8 @@ extern int drawLine(int x0, int y0, int x1, int y1, Bitmap *bitmap, RGB rgb);
  * @param y Y coordinate of the Pixel inside the Bitmap
  * @param bitmap The Bitmap struct in which the Pixel should be set.
  * @param rgb An RGB struct representing the color the pixel should be set to.
- * @return 0 if Pixel was successfully set, else 1
+ * @return 0 if pixel was successfully set
+ *         1 if pixel is out of bounds
  */
 extern int setPixel(int x, int y, Bitmap *bitmap, RGB rgb);
 
